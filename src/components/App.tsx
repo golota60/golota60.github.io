1c46b65cddb5b5c9ad0f3e573794eb5f110e0892
golota60 <37072867+golota60@users.noreply.github.com>
2020-05-09 16:33:59 +0200

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => {
+    return (
+        <div className="App">
+            bruh
+        </div>
+    );
+}
+export default App;
