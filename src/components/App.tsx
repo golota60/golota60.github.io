970c7f5bffac94b72bb7251b9af6e321712cdd4e
Szymon Wiszczuk <golota60@gmail.com>
2020-06-15 19:20:37 +0200

    implemented main func

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,39 @@
 import React from 'react';
 import './App.scss';
 import coffee from '../assets/coffee.svg';
+import github from '../assets/github.svg';
 import TextWrapper from './generic/TextWrapper';
 import TextLooper from './generic/TextLooper';
 
 const App = () => {
 
-  const technologies = ['javascript', 'typescript', 'React', 'CSS', 'SASS'];
+    const technologies = [
+        'Javascript',
+        'Typescript',
+        'React',
+        'CSS',
+        'SASS',
+        'Node',
+        'Express',
+        'UI/UX'
+    ];
 
-  return (
-    <div className="App">
-      <TextWrapper textType="h1">I'm Szymon</TextWrapper>
-      <TextWrapper textType="h2">And I really like  
-        <TextLooper msTransitionTime={2000} textArray={technologies}></TextLooper>
-      </TextWrapper>
-      <img src={coffee} className="coffee-image"></img>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="information">
+                <TextWrapper className="information__name" textType="h1">I'm Szymon</TextWrapper>
+                <TextWrapper textType="h2" className="information__technologies">And I really like&nbsp;
+                    <TextLooper msTransitionTime={2000} textArray={technologies}></TextLooper>
+                </TextWrapper>
+                <div className="information__github">
+                    <TextWrapper textType="h2" color="light-gray">How much? See for yourself</TextWrapper>
+                    <a href="https://www.github.com/golota60" target="_blank">
+                        <img src={github} className="image__github"></img>
+                    </a>
+                </div>
+                <img src={coffee} className="image__coffee"></img>
+            </div>
+        </div>
+    );
 }
 export default App;
--- src/components/generic/TextLooper.tsx
+++ src/components/generic/TextLooper.tsx
@@ -10,28 +10,18 @@ interface TextLooperInterface {
 const TextLooper = ({textArray, msTransitionTime: transitionTime}: TextLooperInterface) => {
   const [currentText, setCurrentText] = useState(textArray[0]);
 
-function returnNextArrayItem(array: Array<any>, currentItem: any): any {
+const returnNextArrayItem = (array: Array<any>, currentItem: any): any => {
   const itemIndex = array.indexOf(currentItem);
-  return (itemIndex === array.length - 1) ? array[0] : array[itemIndex + 1];
+  return (array.indexOf(currentItem) === array.length - 1) ? array[0] : array[itemIndex + 1];
 }
 
-
-
-  useEffect(() => {  
-    console.log('use effect')
-    const textInterval = setInterval(() => {
-    setCurrentText(returnNextArrayItem(textArray, currentText));
+    setTimeout(() => {
+      setCurrentText(returnNextArrayItem(textArray, currentText));
     }, transitionTime);
-    return () => {
-      console.log(' return use effect')
-      clearInterval(textInterval);
-    }
-  },[]);
 
   return (
     <>
-    {console.log('rerender')}
-    <TextWrapper>{currentText}</TextWrapper>
+    <TextWrapper textType="h2">{currentText}</TextWrapper>
     </>
   );
 }
