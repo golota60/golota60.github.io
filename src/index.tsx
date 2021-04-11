50e73a4cc3dc443a662a59c4bdf2eb9a9dd83210
Szymon Wiszczuk <golota60@gmail.com>
2021-04-11 17:11:21 +0200

    added smoothscroll polyfill

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { BrowserRouter } from "react-router-dom";
+import smoothscroll from "smoothscroll-polyfill";
 
-ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('root'));
+smoothscroll.polyfill();
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
