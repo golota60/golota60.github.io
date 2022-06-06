2c9b659b344ed3e3ad7294fcc7c96793cfe2711f
Szymon Wiszczuk <golota60@gmail.com>
2022-06-06 22:16:21 +0200

    add termi clicker

--- src/components/generic/TextWrapper.tsx
+++ src/components/generic/TextWrapper.tsx
@@ -1,11 +1,11 @@
-import React, { ReactNode } from 'react';
-import './TextWrapper.scss';
+import React, { ReactNode } from "react";
+import "./TextWrapper.scss";
 
 interface TextWrapperProps {
   className?: string;
-  color?: 'black' | 'light-gray' | 'gray' | 'dark-gray' | 'white' | 'red';
+  color?: "black" | "light-gray" | "gray" | "dark-gray" | "white" | "red";
   children: ReactNode;
-  textType?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+  textType?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
   onClick?: () => void;
 }
 
@@ -14,28 +14,28 @@ const TextWrapper = ({
   color,
   children,
   textType,
-  onClick
+  onClick,
 }: TextWrapperProps) => {
   function createFontSize() {
-    let fontSize = '1.6rem';
+    let fontSize = "1.6rem";
     switch (textType) {
-      case 'h1':
-        fontSize = '3.2rem';
+      case "h1":
+        fontSize = "3.2rem";
         break;
-      case 'h2':
-        fontSize = '2.4rem';
+      case "h2":
+        fontSize = "2.4rem";
         break;
-      case 'h3':
-        fontSize = '1.872rem';
+      case "h3":
+        fontSize = "1.872rem";
         break;
-      case 'h4':
-        fontSize = '1.6rem';
+      case "h4":
+        fontSize = "1.6rem";
         break;
-      case 'h5':
-        fontSize = '1.328rem';
+      case "h5":
+        fontSize = "1.328rem";
         break;
-      case 'h6':
-        fontSize = '1.072rem';
+      case "h6":
+        fontSize = "1.072rem";
         break;
       default:
         break;
@@ -47,8 +47,8 @@ const TextWrapper = ({
   function createClassName() {
     const newClassName: Array<string> = [];
     className && newClassName.push(className);
-    color ? newClassName.push(color) : newClassName.push('dark-gray');
-    return newClassName.join(' ');
+    color ? newClassName.push(color) : newClassName.push("dark-gray");
+    return newClassName.join(" ");
   }
 
   return (
